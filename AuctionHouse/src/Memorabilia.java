b345aee8151317a6d3c1a361ae3b6708d1f49ce7
Favour Sukat <fds4000@hw.ac.uk>
2024-10-06 00:30:32 +0100

    feat: add Memorabilia class with attributes #1

---
+++ AuctionHouse/src/Memorabilia.java
@@ -0,0 +1,11 @@
+public class Memorabilia {
+    private String id;
+    private String personalityName;
+    private String personalityOccupation;
+    private String objectType;
+    private boolean isAutographed;
+    private int estimatedYear;
+    private String owner;
+    private String condition;
+    private double startingPrice;
+}
