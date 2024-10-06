221269ddf8f416bf7f3e9bffd048000d71c55d92
Favour Sukat <fds4000@hw.ac.uk>
2024-10-06 01:01:42 +0100

    feat: add MemorabiliaCollection class with items list #1

---
+++ AuctionHouse/src/MemorabiliaCollection.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class MemorabiliaCollection {
+    private ArrayList<Memorabilia> items;
+
+    //constructor to initialize items
+    public MemorabiliaCollection(){
+        items = new ArrayList<Memorabilia>();
+    }
+
+}
