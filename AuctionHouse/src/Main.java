20ceee6b1fe1c842fb3ece5d45005eddb9eb882e
Favour Sukat <fds4000@hw.ac.uk>
2024-10-06 02:00:24 +0100

    fix: Change the Main class to test the newly created classes #1

--- AuctionHouse/src/Main.java
+++ AuctionHouse/src/Main.java
@@ -1,6 +1,25 @@
 public class Main{
 
     public static void main(String[] args){
-        System.out.println("Hello Auction House");
+
+        MemorabiliaCollection collection = new MemorabiliaCollection();
+
+        // Create some memorabilia items
+        Memorabilia item1 = new Memorabilia("001", "Elvis Presley", "Singer", "Guitar", true, 1956, "Alice", "Mint", 20000.00);
+        Memorabilia item2 = new Memorabilia("002", "Marilyn Monroe", "Actress","Dress", false, 1962, "Bob", "Good", 15000.00);
+        // Duplicate ID test
+        Memorabilia item3 = new Memorabilia("001", "Elvis Presley", "Singer", "Guitar", true, 1956, "Alice", "Mint", 20000.00);
+
+        // Add items to the collection
+        collection.addItem(item1);
+        collection.addItem(item2);
+        collection.addItem(item3); // Attempt to add duplicate
+
+        // Display the number of items
+        System.out.println("Total items in collection: " + collection.getNumberOfItems());
+
+
+        // Display all items using displayAllItems method
+        collection.displayAllItems();
     }
-}
+    }
