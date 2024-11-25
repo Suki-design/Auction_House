32998c7b3520d6b1847ea39419cee61aa3aa88f0
Favour Sukat <fds4000@hw.ac.uk>
2024-11-25 09:23:52 +0000

    refactor:remove Id Comparator class #9

--- AuctionHouse/src/auctionhouse/model/IdComparator.java
+++
@@ -1,8 +0,0 @@
-package auctionhouse.model;
-import java.util.Comparator;
-public class IdComparator implements Comparator<Collectible>{
-    //ascending sort
-    public int compare(Collectible c1, Collectible c2){
-        return c1.getId().compareTo(c2.getId());
-    }
-}
